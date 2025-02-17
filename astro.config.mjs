a869da0ad0b3753dd0b3f8a953fba562fcd8b464
Raweeroj Thongdee <fewlove.007@gmail.com>
2025-02-17 17:04:06 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: {
+      en: "Documentation",
+      th: "คู่มือการใช้งาน",
+    },
+    defaultLocale: "root",
+    locales: {
+      root: {
+        label: "English",
+        lang: "en",
+      },
+      // Thai docs in 'src/content/docs/th'
+      th: {
+        label: "ไทย",
+        lang: "th",
+      },
+    },
+    social: {
+      github: "https://github.com/withastro/starlight",
+    },
+  }), markdoc()],
 });
